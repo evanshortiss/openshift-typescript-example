f6b257abc9f392a1b8b71991a5de802725d13175
Evan Shortiss <evanshortiss@gmail.com>
2021-05-11 16:40:21 +0100

    feat: use cooler emojis

--- scripts/s2i.sh
+++ scripts/s2i.sh
@@ -2,6 +2,12 @@
 
 IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-quay.io/evanshortiss/openshift-s2i-typescript-example:latest}
 
+# Don't want to copy in local node_modules to the build
 rm -rf node_modules/
 
-s2i build -e HUSKY_SKIP_INSTALL=1 -c . registry.access.redhat.com/ubi8/nodejs-12 ${IMAGE_REPOSITORY}
+# Build local codebase using source-to-image. Skip the husky hooks
+# installation using env vars. Use red hat ubi8 nodejs 12 as the base
+# image and tag using the given tag name
+s2i build -c . \
+-e HUSKY_SKIP_INSTALL=1 \
+registry.access.redhat.com/ubi8/nodejs-12 ${IMAGE_REPOSITORY}
